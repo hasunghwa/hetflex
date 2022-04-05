f09ef2418a9ffe0496f29056d52e7aa9b35c308c
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-04-05 16:06:32 +0900

    💄 Style : body 폰트 변경

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {createGlobalStyle} from "styled-components"
 import {QueryClient, QueryClientProvider} from "react-query"
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -58,7 +58,7 @@ table {
 }
 body {
   font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   color: white;
   line-height: 1.2;
   background-color: black;
